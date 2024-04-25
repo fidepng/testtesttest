667b140c1f7d72fda48a07a38c5226be59c5d47f
fidepng <163156886+fidepng@users.noreply.github.com>
2024-04-25 18:15:59 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ function animateOnScroll() {
 
 window.addEventListener('load', animateOnScroll);
 
-// animasi typing header
+// Animasi typing header
 const typedTextSpan = document.querySelector('.typed-text');
 const textArray = ['Halo! Selamat Datang. '];
 let textArrayIndex = 0;
@@ -34,7 +34,7 @@ function type() {
     setTimeout(deleteText, 3000); // Jeda sebelum menghapus teks
   }
 }
- 
+
 function deleteText() {
   if (charIndex > 0) {
     typedTextSpan.textContent = textArray[textArrayIndex].slice(0, charIndex - 1);
@@ -53,47 +53,66 @@ document.addEventListener('DOMContentLoaded', function() {
   setTimeout(type, 1000); // Jeda sebelum memulai animasi
 });
 
-// Lightbox gallery
-const gallery = document.querySelector('.image-grid');
+// Membuat elemen lightbox hanya sekali saat dimuat
 const lightbox = document.createElement('div');
 lightbox.classList.add('lightbox');
 document.body.appendChild(lightbox);
 
-gallery.addEventListener('click', e => {
-  if (e.target.tagName === 'IMG') {
-    const img = document.createElement('img');
-    img.src = e.target.src;
-    lightbox.appendChild(img);
+// Menambahkan event listener pada setiap gambar di galeri
+const galleryImages = document.querySelectorAll('.image-grid img');
+galleryImages.forEach(image => {
+  image.addEventListener('click', () => {
+    const imgSrc = image.src;
+    const lightboxImage = document.createElement('img');
+    lightboxImage.src = imgSrc;
+    lightbox.innerHTML = '';
+    lightbox.appendChild(lightboxImage);
     lightbox.style.display = 'flex';
-  }
+  });
 });
 
+// Menambahkan event listener untuk menutup lightbox saat di-klik
 lightbox.addEventListener('click', () => {
-  lightbox.innerHTML = '';
   lightbox.style.display = 'none';
 });
 
-// Pagination pada Blog
+// Data artikel blog
+const blogs = [
+  {
+    title: "Hasil Swiss Open 2024: Gregoria Kalah Dramatis di Final",
+    date: "24 Maret 2024",
+    author: "Novitasari Dewi Salusi",
+    image: "asset/img/gregoria-mariska-tunjung_169.jpeg",
+    description: "Gregoria Mariska Tunjung harus puas jadi runner-up Swiss Open 2024 usai kalah dari Carolina Marin. Gregoria kalah meski sempat dapat match point lebih dulu. Dalam pertandingan final Swiss Open 2024 di St. Jakobshalle, Basel, Minggu (24/3/2024) malam WIB, Gregoria melewati duel ketat sepanjang tiga gim melawan Marin. Setelah bertarung selama 1 jam 21 menit, Gregoria kalah 19-21, 21-13, 20-22 dari juara dunia tiga kali itu.",
+    url: "https://sport.detik.com/raket/d-7259457/hasil-swiss-open-2024-gregoria-kalah-dramatis-di-final"
+  },
+  {
+    title: "Mengapa Revolusi Industri Penting? Dampaknya Pada Masa Lalu, Kini, dan Masa Depan",
+    date: "10 April 2024",
+    author: "Ilman Kava Billah",
+    image: "asset/img/revolusi-industri-660189b7c57afb423f32acd2.jpg",
+    description: "Salah satu peristiwa penting dalam sejarah adalah revolusi industri, yang mengubah cara orang hidup, bekerja, dan berinteraksi satu sama lain. Pertama-tama, revolusi industri memainkan peran penting dalam mendorong kemajuan ekonomi. Ini menghasilkan peningkatan efisiensi produksi dan penyebaran teknologi baru.",
+    url: "https://www.kompasiana.com/ilmankava27/66018856de948f50ff6a55f2/mengapa-revolusi-industri-penting-dampaknya-pada-masa-lalu-kini-dan-masa-depan"
+  },
+  {
+    title: "Kilas Balik Sejarah Seni Digital: Dari John Whitney Hingga NFT",
+    date: "15 April 2024",
+    author: "Tri Wahyu Prasetyo",
+    image: "asset/img/screenshot-2023-01-23-162549jpg-20230304021600.jpg",
+    description: "Nationalgeographic.co.id - Kemegahan seni digital dewasa ini tidak lepas dari perkembangan teknologi, dan eksplorasi seniman-seniman terdahulu. Seperti kata pepatah, 'perjalanan seribu mil dimulai dengan langkah pertama.'",
+    url: "https://nationalgeographic.grid.id/read/133715199/kilas-balik-sejarah-seni-digital-dari-john-whitney-hingga-nft?page=all"
+  }
+];
+
 const blogList = document.querySelector('.blog-list');
 const prevButton = document.querySelector('.pagination .prev');
 const nextButton = document.querySelector('.pagination .next');
 const pageInfo = document.querySelector('.pagination .page-info');
 
 let currentPage = 1;
-const itemsPerPage = 1;
-let blogs = [
-  // Data artikel blog
-  { title: "Hasil Swiss Open 2024: Gregoria Kalah Dramatis di Final - Novitasari Dewi Salusi",
-    description: "Gregoria Mariska Tunjung harus puas jadi runner-up Swiss Open 2024 usai kalah dari Carolina Marin. Gregoria kalah meski sempat dapat match point lebih dulu. Dalam pertandingan final Swiss Open 2024 di St. Jakobshalle, Basel, Minggu (24/3/2024) malam WIB, Gregoria melewati duel ketat sepanjang tiga gim melawan Marin. Setelah bertarung selama 1 jam 21 menit, Gregoria kalah 19-21, 21-13, 20-22 dari juara dunia tiga kali itu.",
-    url: "https://sport.detik.com/raket/d-7259457/hasil-swiss-open-2024-gregoria-kalah-dramatis-di-final" },
-  { title: "Mengapa Revolusi Industri Penting? Dampaknya Pada Masa Lalu, Kini, dan Masa Depan - Ilman Kava Billah",
-    description: "Salah satu peristiwa penting dalam sejarah adalah revolusi industri, yang mengubah cara orang hidup, bekerja, dan berinteraksi satu sama lain. Pertama-tama, revolusi industri memainkan peran penting dalam mendorong kemajuan ekonomi. Ini menghasilkan peningkatan efisiensi produksi dan penyebaran teknologi baru.",
-    url: "https://www.kompasiana.com/ilmankava27/66018856de948f50ff6a55f2/mengapa-revolusi-industri-penting-dampaknya-pada-masa-lalu-kini-dan-masa-depan" },
-  { title: "Kilas Balik Sejarah Seni Digital: Dari John Whitney Hingga NFT - Tri Wahyu Prasetyo",
-    description: "Nationalgeographic.co.id - Kemegahan seni digital dewasa ini tidak lepas dari perkembangan teknologi, dan eksplorasi seniman-seniman terdahulu. Seperti kata pepatah, 'perjalanan seribu mil dimulai dengan langkah pertama.' ",
-    url: "https://nationalgeographic.grid.id/read/133715199/kilas-balik-sejarah-seni-digital-dari-john-whitney-hingga-nft?page=all" },
-];
+const itemsPerPage = 2; // Jumlah artikel yang ditampilkan per halaman
 
+// Fungsi untuk menampilkan artikel blog
 function showBlogs(blogs, page) {
   const startIndex = (page - 1) * itemsPerPage;
   const endIndex = startIndex + itemsPerPage;
@@ -103,31 +122,39 @@ function showBlogs(blogs, page) {
   blogsToShow.forEach(blog => {
     const article = document.createElement('article');
     article.innerHTML = `
-    <h3 class="title">${blog.title}</h3>
-     <p class="description">${blog.description}</p>
-     <a href="${blog.url}" target="_blank" rel="noopener noreferrer">Baca selengkapnya...</a>
-   `;
-   blogList.appendChild(article);
- });
+      <header>
+        <h3 class="title">${blog.title}</h3>
+        <p class="meta">${blog.date} | ${blog.author}</p>
+      </header>
+      <img src="${blog.image}" alt="Gambar Artikel">
+      <p class="description">${blog.description}</p>
+      <footer>
+        <a href="${blog.url}" class="read-more" target="_blank" rel="noopener noreferrer">Baca Selengkapnya</a>
+      </footer>
+    `;
+    blogList.appendChild(article);
+  });
 }
 
+// Fungsi untuk memperbarui tampilan pagination
 function updatePagination() {
- const totalPages = Math.ceil(blogs.length / itemsPerPage);
- prevButton.disabled = currentPage === 1;
- nextButton.disabled = currentPage === totalPages;
- pageInfo.textContent = `Page ${currentPage} of ${totalPages}`;
+  const totalPages = Math.ceil(blogs.length / itemsPerPage);
+  prevButton.disabled = currentPage === 1;
+  nextButton.disabled = currentPage === totalPages;
+  pageInfo.textContent = `Page ${currentPage} of ${totalPages}`;
 }
 
+// Event listener untuk tombol pagination
 prevButton.addEventListener('click', () => {
- currentPage--;
- showBlogs(blogs, currentPage);
- updatePagination();
+  currentPage--;
+  showBlogs(blogs, currentPage);
+  updatePagination();
 });
 
 nextButton.addEventListener('click', () => {
- currentPage++;
- showBlogs(blogs, currentPage);
- updatePagination();
+  currentPage++;
+  showBlogs(blogs, currentPage);
+  updatePagination();
 });
 
 // Inisialisasi tampilan awal
@@ -139,11 +166,11 @@ const navToggle = document.querySelector('.nav-toggle');
 const navLinks = document.querySelector('.nav__items');
 
 function myFunction(x) {
-    x.classList.toggle("change");
-    navLinks.classList.toggle("show");
+  x.classList.toggle("change");
+  navLinks.classList.toggle("show");
 }
 
-// nav sticky
+// Nav sticky
 const nav = document.querySelector('nav');
 const logo = document.querySelector('.logo');
 
